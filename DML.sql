ffecbb359b037a0b4cd8774a744289ac2d21cc5b
ana-111 <anacarolinaamaralsilva2003@gmail.com>
2020-08-04 15:50:56 -0300

    DML finalizado

---
+++ DML.sql
@@ -0,0 +1,10 @@
+/*DML - Data Manipulation Language*/
+USE empresa;
+
+/*Inserir Tabela*/
+SELECT * FROM Cliente;
+
+INSERT INTO Cliente (Nome, CPF) VALUES ('Ana', 39189);
+
+/*DQL simples*/
+SELECT * FROM Cliente;
